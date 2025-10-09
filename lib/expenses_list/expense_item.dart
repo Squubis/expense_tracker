305ea7b0297a5ee9cfd66df0af65caf8c4cfaef0
Shlomie Garcia <shlomiegarcia@gmail.com>
2025-10-09 12:29:43 -0400

    Moved files around: created a expense_list folder and moved expenses_list file in there to create organization. Buio;t a expense_item file.

---
+++ lib/expenses_list/expense_item.dart
@@ -0,0 +1,14 @@
+import 'package:expense_tracker/material/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget{
+  const ExpenseItem({super.key, required this.expense});
+  final Expense expense; 
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}  
