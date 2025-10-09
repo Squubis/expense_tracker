baa78dcd78f560b73ade5852657781bf4ec59a02
Shlomie Garcia <shlomiegarcia@gmail.com>
2025-10-09 12:17:46 -0400

    initial changes: Created Model, expenses list, expenses page.

---
+++ lib/expenses.dart
@@ -0,0 +1,26 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/material/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget{
+  const Expenses({super.key});
+
+  State<Expenses> createState(){
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses>
+{
+  final List <Expense> _registereExpenses = [
+    Expense(title: "Cheeseburger", amount: 12.45, date: DateTime.now(), category: Category.food), Expense(title: "Pizza", amount: 5.00, date: DateTime.now(), category: Category.food)
+  ];
+    @override
+    Widget build(BuildContext context){
+      return Scaffold(
+        body: Column(
+          children: [Text("Chart"), ExpensesList(expenses: _registereExpenses)],
+        )
+      );
+    }
+  }
