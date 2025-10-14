8a98f049686b83c6e4ca5cc08b125bfce0794d5c
Shlomie Garcia <shlomiegarcia@gmail.com>
2025-10-14 12:05:31 -0400

    Added formattedDate into the code by installing intl.

--- lib/material/expense.dart
+++ lib/material/expense.dart
@@ -1,10 +1,25 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
 const uuid = Uuid();
+final formatter = DateFormat.yMd();
+
+
 enum Category {food, travel, leisure, work}
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.bus_alert,
+  Category.leisure: Icons.tv,
+  Category.work: Icons.work,
+};
 
 class Expense {
    Expense({
-    required this.title, required this.amount, required this.date, required this.category,
+    required this.title, 
+    required this.amount, 
+    required this.date, 
+    required this.category,
   }): id = uuid.v4();
 
   final String id;
@@ -13,5 +28,8 @@ class Expense {
   final DateTime date;
   final Category category;
 
+  String get formattedDate{
+    return formatter.format(date);
+  }
   
 }
