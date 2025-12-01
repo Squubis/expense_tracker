7090bb39b1a8fe437489ab65e89f4f6a56f822f6
Shlomie Garcia <shlomiegarcia@gmail.com>
2025-12-01 17:39:56 -0500

    made chart bar to calculate expenses visually

---
+++ lib/widgets/chart/chart_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget{
+  const ChartBar ({super.key, required this.fill});
+
+  final double fill;
+  Widget build(BuildContext context){
+    
+    final isDarkMode = 
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    return Expanded(
+      child: Padding(padding: EdgeInsetsGeometry.symmetric(horizontal: 4),
+        child: FractionallySizedBox(
+          heightFactor: fill,
+          child: DecoratedBox(decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: const BorderRadius.vertical(top:Radius.circular(8)),
+            color: isDarkMode
+            ? Theme.of(context).colorScheme.secondary
+            : Theme.of(context).colorScheme.primary
+          ),)
+        )
+      ),
+    );
+    
+    return Text("bar");
+  }
+}
